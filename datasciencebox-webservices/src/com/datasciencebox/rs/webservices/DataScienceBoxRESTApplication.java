a40acc4c775b8eb9690a7526f2095749e5db4350
fuzzyanalysis <fuzzyanalysis@gmail.com>
2014-04-25 14:32:25 +1000

    Shannon - initial check-in

---
+++ datasciencebox-webservices/src/com/datasciencebox/rs/webservices/DataScienceBoxRESTApplication.java
@@ -0,0 +1,22 @@
+package com.datasciencebox.rs.webservices;
+
+import java.util.Set;
+import java.util.HashSet;
+import javax.ws.rs.core.Application;
+
+public class DataScienceBoxRESTApplication extends Application {
+
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
+	public DataScienceBoxRESTApplication(){
+	     singletons.add(new DataScienceBoxResource());
+	}
+	@Override
+	public Set<Class<?>> getClasses() {
+	     return empty;
+	}
+	@Override
+	public Set<Object> getSingletons() {
+	     return singletons;
+	}
+}
---
+++ datasciencebox-webservices/src/com/datasciencebox/rs/webservices/DataScienceBoxResource.java
@@ -0,0 +1,42 @@
+package com.datasciencebox.rs.webservices;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.maxmind.geoip.Location;
+import com.maxmind.geoip.LookupService;
+
+@Path("/DataScienceBoxRESTApplication")
+public class DataScienceBoxResource {
+
+	@Path("/geolocation/{ip}")
+	@GET()
+	@Produces("application/json")
+	public Location getGeolocation(@PathParam("ip") final String ip) {
+	    
+		// get Server location
+		
+		String currentDir = new File(".").getAbsolutePath();
+		System.out.println(currentDir);
+		
+		Location location = null;
+		
+		try {
+			LookupService cl = new LookupService("GeoLiteCity.dat",
+					LookupService.GEOIP_MEMORY_CACHE
+							| LookupService.GEOIP_CHECK_CACHE);
+			location = cl.getLocation(ip);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		return location;
+		
+	}
+}
